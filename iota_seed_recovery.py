50f118c3f70999fc9d151172cc78fa405b97ad63
Foffex <40924386+Foffex@users.noreply.github.com>
2020-01-09 22:05:14 +0100

    updated iota_seed_recovery
    Removed "import pprint" as it's not used

--- iota_seed_recovery.py
+++ iota_seed_recovery.py
@@ -1,5 +1,4 @@
 from iota import Iota
-import pprint
 
 
 characters = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '9')
